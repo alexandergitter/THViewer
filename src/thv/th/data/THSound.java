5b8f5952858969afec184db96ddf981811f5a77a
Stephen E. Baker <baker.stephen.e@gmail.com>
2025-07-21 22:23:30 -0400

    Fix loading and playing sounds

--- src/thv/th/data/THSound.java
+++ src/thv/th/data/THSound.java
@@ -10,27 +10,24 @@
 
 package thv.th.data;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.net.MalformedURLException;
+import java.io.InputStream;
 import java.util.Vector;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
+
+import thv.util.ABuffer;
 
 public class THSound {
     
-    private File soundFile;
+    private ABuffer soundBuffer;
     private Vector<Sample> samples;
     
     /** Creates a new instance of THSound */
-    public THSound(File soundFile) {
-        this.soundFile = soundFile;
+    public THSound(ABuffer soundBuffer) {
+        this.soundBuffer = soundBuffer;
         this.samples = new Vector<Sample>();
     }
     
@@ -44,28 +41,29 @@ public class THSound {
     
     public void play(Sample s) {
         try {
-        FileInputStream fis = new FileInputStream(soundFile);
-        fis.getChannel().position(s.getPosition());
-        // From file
-        AudioInputStream stream = AudioSystem.getAudioInputStream(fis);
-    
-        // At present, ALAW and ULAW encodings must be converted
-        // to PCM_SIGNED before it can be played
-        AudioFormat format = stream.getFormat();
+            InputStream fis = soundBuffer.createInputStream();
+            fis.skip(s.getPosition());
+            
+            // From file
+            AudioInputStream stream = AudioSystem.getAudioInputStream(fis);
         
-        // Create the clip
-        DataLine.Info info = new DataLine.Info(
-            Clip.class, stream.getFormat(), ((int)stream.getFrameLength()*format.getFrameSize()));
-        Clip clip = (Clip) AudioSystem.getLine(info);
-    
-        // This method does not return until the audio file is completely loaded
-        clip.open(stream);
-    
-        // Start playing
-        clip.start();
-        } catch (MalformedURLException e) {
-        } catch (IOException e) {
-        } catch (LineUnavailableException e) {
-        } catch (UnsupportedAudioFileException e) {}
+            // At present, ALAW and ULAW encodings must be converted
+            // to PCM_SIGNED before it can be played
+            AudioFormat format = stream.getFormat();
+
+            // Create the clip
+            DataLine.Info info = new DataLine.Info(
+                Clip.class, stream.getFormat(), ((int)stream.getFrameLength()*format.getFrameSize()));
+            Clip clip = (Clip) AudioSystem.getLine(info);
+        
+            // This method does not return until the audio file is completely loaded
+            clip.open(stream);
+        
+            // Start playing
+            clip.start();
+        } catch (Exception e) {
+            System.err.println("Error playing sound: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
